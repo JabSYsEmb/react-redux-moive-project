455838defe5fe7126955139853ca3a716dac06ab
androidmini9x <androidmini92@gmail.com>
2022-03-03 03:40:15 +0300

    Build Home Page body

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,17 +4,17 @@ const MovieCard = ({ movie }) => {
     const { bookmark } = useSelector(state => state.movies);
 
     return (
-        <div class="inline-block text-center group md:w-200 mb-6 sm:max-w-fit">
-            <div class="relative text-center">
-                <img class="group-hover:blur-sm shadow" src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title} />
-                <span class="absolute px-2 left-1 top-1 rounded text-white bg-orange-400 inline-block">{movie.vote_average}</span>
-                <p class="bg-gray-400 bg-opacity-80 transition duration-500 opacity-0 group-hover:opacity-100 absolute inset-0 z-10 flex flex-col justify-center items-center text-center text-1xl text-white px-4">
+        <div className="inline-block text-center group md:w-200 mb-6 sm:max-w-fit">
+            <div className="relative text-center">
+                <img className="group-hover:blur-sm shadow" src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title} />
+                <span className="absolute px-2 left-1 top-1 rounded text-white bg-orange-400 inline-block">{movie.vote_average}</span>
+                <p className="bg-gray-400 bg-opacity-80 transition duration-500 opacity-0 group-hover:opacity-100 absolute inset-0 z-10 flex flex-col justify-center items-center text-center text-1xl text-white px-4">
                     <span className="font-bold text-2xl">{movie.title}</span>
                     {movie.overview.length > 200 ? movie.overview.substr(0, 200) + "..." : movie.overview}
                 </p>
             </div>
-            <h1 class="font-medium text-1xl text-white px-2">{movie.title}</h1>
-            <span class="text-xs text-green-100">{new Date(movie.release_date).toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}</span>
+            <h1 className="font-medium text-1xl text-white px-2">{movie.title}</h1>
+            <span className="text-xs text-green-100">{new Date(movie.release_date).toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}</span>
         </div>
     );
 }
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -13,13 +13,13 @@ const Home = () => {
     console.log(popular);
 
     return <div className="mx-auto">
-        <h1 class="text-4xl text-purple-800 text-left mb-6 bg-white font-bold p-6 shadow"><span class="inline-block align-middle h-14 w-2 bg-yellow-300"></span> Popular Movies</h1>
+        <h1 className="text-4xl text-purple-800 text-left mb-6 bg-white font-bold p-6 shadow"><span className="inline-block align-middle h-14 w-2 bg-yellow-300"></span> Popular Movies</h1>
         <div className="grid grid-cols-1 justify-items-center xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
             {popular.map(movie => {
                 return <MovieCard movie={movie} />
             })}
         </div>
-        <h1 class="text-4xl text-purple-800 text-left mb-6 bg-white font-bold p-6 shadow"><span class="inline-block align-middle h-14 w-2 bg-yellow-300"></span> Trending Movies</h1>
+        <h1 className="text-4xl text-purple-800 text-left mb-6 bg-white font-bold p-6 shadow"><span className="inline-block align-middle h-14 w-2 bg-yellow-300"></span> Trending Movies</h1>
         <h3></h3>
         <div className="grid grid-cols-4 justify-items-center">
             {trending.map(movie => {
